79d24be9facb5652fb6009a59165de642da3723e
Geeleed <surasak.kaewpho@gmail.com>
2023-08-01 12:03:19 +0700

    deploy สำเร็จแล้วแต่ขอแก้เพิ่มให้รับเป็น str

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ async def root():
     return {"message": "Hello World"} 
 
 @app.get("/{id}")  # decorator to define a route for GET method on "/{id}" path
-async def root(id):
+async def root(id:str):
     return {"message": f"Hello World {id}"}  # Return a JSON response with the dynamic "id"
 
 # uvicorn api:app --port 8001 --reload
