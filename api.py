9cfeb83c026ee5b05ca540b7bede023349de10da
Geeleed <surasak.kaewpho@gmail.com>
2023-08-01 11:20:26 +0700

    อัปขึ้น git เพื่อทดสอบ deploy FastAPI

---
+++ api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")  
+async def root(): 
+    return {"message": "Hello World"} 
+
+@app.get("/{id}")  # decorator to define a route for GET method on "/{id}" path
+async def root(id):
+    return {"message": f"Hello World {id}"}  # Return a JSON response with the dynamic "id"
+
+# uvicorn api:app --port 8001 --reload
