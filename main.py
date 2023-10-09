d9ebf4b5049956fca4857fb98c696fab204fc6be
Geeleed <surasak.kaewpho@gmail.com>
2023-10-10 04:46:26 +0700

    update

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 # ใช้คำสั่งนี้เพื่อเปิดเซิฟเวอร์ uvicorn main:app --port 8001 --reload
 
 from fastapi import FastAPI, UploadFile
-# from starlette.responses import StreamingResponse
-# from starlette.responses import FileResponse
-# import requests
-# import tempfile
 
 app = FastAPI()
 
@@ -16,43 +12,6 @@ async def geeleed():
 @app.get("/{id}/")  # decorator to define a route for GET method on "/{id}" path
 async def test(id:str):
     return {"message": f"Hello World {id}"}  # Return a JSON response with the dynamic "id"
-
-
-# # API ดาวน์โหลดรูปภาพจาก URL
-# @app.get("/download/{image_url:path}")
-# async def download_image(image_url: str):
-#     # ดาวน์โหลดรูปภาพจาก URL
-#     response = requests.get(image_url, stream=True)
-    
-#     # ตรวจสอบว่าการดาวน์โหลดเสร็จสิ้นและสำเร็จ
-#     if response.status_code == 200:
-#         # สร้างการตอบสนอง StreamingResponse โดยใช้ response.iter_content เพื่อส่งข้อมูลไปยังการตอบสนองในรูปแบบ Streaming
-#         headers = {"Content-Disposition": f"attachment; filename=downloaded_image.jpg"}  # ระบุชื่อไฟล์ที่ต้องการให้ผู้ใช้ดาวน์โหลด
-#         return StreamingResponse(iter(response.iter_content(chunk_size=128)), media_type="image/jpeg", headers=headers)
-#     else:
-#         # หากไม่สามารถดาวน์โหลดรูปภาพได้
-#         return {"error": "Unable to download image"}
-
-# # API ดาวน์โหลดรูปภาพจาก URL
-# @app.get("/download/{image_url:path}")
-# async def download_image(image_url: str):
-#     # ดาวน์โหลดรูปภาพจาก URL
-#     response = requests.get(image_url, stream=True)
-    
-#     # ตรวจสอบว่าการดาวน์โหลดเสร็จสิ้นและสำเร็จ
-#     if response.status_code == 200:
-#         # สร้างไฟล์ชั่วคราวเพื่อเก็บข้อมูลรูปภาพ
-#         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-#             for chunk in response.iter_content(chunk_size=128):
-#                 temp_file.write(chunk)
-        
-#         # สร้างการตอบสนอง FileResponse โดยระบุชื่อไฟล์ที่ต้องการให้ผู้ใช้ดาวน์โหลด
-#         file_name = "downloaded_image.jpg"  # ระบุชื่อไฟล์ที่ต้องการให้ผู้ใช้ดาวน์โหลด
-#         return FileResponse(temp_file.name, headers={"Content-Disposition": f"attachment; filename={file_name}"})
-#     else:
-#         # หากไม่สามารถดาวน์โหลดรูปภาพได้
-#         return {"error": "Unable to download image"}
-
 
 # api เข้ารหัสข้อความ sha256
 import hashlib
@@ -101,18 +60,18 @@ async def convert_base(number,from_base=10,to_base=16):
         dec //= to_base
     return result
 
-# ขอข้อมูล array ของรูปภาพ
+# # ขอข้อมูล array ของรูปภาพ
 from io import BytesIO
-import requests
+# import requests
 from PIL import Image
 import numpy as np
-@app.get('/image2array/{image_url:path}/')
-async def image2array(image_url:str):
-    response = requests.get(image_url)
-    image_data = response.content
-    image = Image.open(BytesIO(image_data))
-    image_array = np.asarray(image)
-    return {"data":image_array.tolist()}
+# @app.get('/image2array/{image_url:path}/')
+# async def image2array(image_url:str):
+#     response = requests.get(image_url)
+#     image_data = response.content
+#     image = Image.open(BytesIO(image_data))
+#     image_array = np.asarray(image)
+#     return {"data":image_array.tolist()}
 
 # รับภาพมาคำนวณสัดส่วนแสงสี
 @app.post('/photo-value/')
@@ -132,86 +91,6 @@ async def photoValue(file:UploadFile):
         "blue_percentage": (blue_mean / 255) * 100,
         "brightness_normalized": brightness_normalized
     }
-
-# อันนี้ lib ใหญ่เกิน vercel ไม่ให้เกิน 250MB
-# # คำนวณ histogram ของภาพ
-# import cv2
-# from scipy.stats import skew, kurtosis
-# @app.post('/photo-hist-data')
-# async def photoHistData(file: UploadFile):
-#     # อ่านไฟล์รูปภาพ
-#     image_bytes = await file.read()
-#     image_np = np.array(Image.open(BytesIO(image_bytes)))
-
-#     # คำนวณฮิสโตแกรมสีแยกตามช่วง canal (R, G, และ B)
-#     hist_red = cv2.calcHist([image_np], [0], None, [256], [0, 256])
-#     hist_green = cv2.calcHist([image_np], [1], None, [256], [0, 256])
-#     hist_blue = cv2.calcHist([image_np], [2], None, [256], [0, 256])
-
-#     # แปลงเป็น grayscale และคำนวณฮิสโตแกรม grayscale
-#     image_gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-#     hist_gray = cv2.calcHist([image_gray], [0], None, [256], [0, 256])
-
-#     # แปลงข้อมูลฮิสโตแกรมให้อยู่ในรูปแบบ NumPy array
-#     hist_data_red = np.squeeze(hist_red).astype(int)
-#     hist_data_green = np.squeeze(hist_green).astype(int)
-#     hist_data_blue = np.squeeze(hist_blue).astype(int)
-#     hist_data_gray = np.squeeze(hist_gray).astype(int)
-
-#     # ค่าเฉลี่ย
-#     m_red = np.mean(hist_data_red)
-#     m_green = np.mean(hist_data_green)
-#     m_blue = np.mean(hist_data_blue)
-#     m_gray = np.mean(hist_data_gray)
-
-#     # ค่าการกระจาย
-#     sd_red = np.std(hist_data_red)
-#     sd_green = np.std(hist_data_green)
-#     sd_blue = np.std(hist_data_blue)
-#     sd_gray = np.std(hist_data_gray)
-
-#     # ค่าความเบ้
-#     skew_red = skew(hist_data_red)
-#     skew_green = skew(hist_data_green)
-#     skew_blue = skew(hist_data_blue)
-#     skew_gray = skew(hist_data_gray)
-
-#     # ค่าความโด่ง
-#     kurt_red = kurtosis(hist_data_red)
-#     kurt_green = kurtosis(hist_data_green)
-#     kurt_blue = kurtosis(hist_data_blue)
-#     kurt_gray = kurtosis(hist_data_gray)
-
-#     return {
-#         "red":{
-#             "mean": m_red,
-#             "sd": sd_red,
-#             "skewness": skew_red,
-#             "kurtosis":kurt_red,
-#             "histogram":hist_data_red.tolist()
-#             },
-#         "green":{
-#             "mean": m_green,
-#             "sd": sd_green,
-#             "skewness": skew_green,
-#             "kurtosis":kurt_green,
-#             "histogram":hist_data_green.tolist()
-#             },
-#         "blue":{
-#             "mean": m_blue,
-#             "sd": sd_blue,
-#             "skewness": skew_blue,
-#             "kurtosis":kurt_blue,
-#             "histogram":hist_data_blue.tolist()
-#             },
-#         "grayscale":{
-#             "mean": m_gray,
-#             "sd": sd_gray,
-#             "skewness": skew_gray,
-#             "kurtosis":kurt_gray,
-#             "histogram":hist_data_gray.tolist()
-#             },
-#     }
 
 def compute_histogram(image_np):
     if image_np.shape[-1] == 3:
