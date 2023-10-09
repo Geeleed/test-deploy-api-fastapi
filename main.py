7b9ed7e121140fad3e818ccbfcad9a301388204b
Geeleed <surasak.kaewpho@gmail.com>
2023-10-09 21:24:06 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse
 import requests
+import tempfile
 
 app = FastAPI()
 
@@ -12,7 +13,7 @@ async def root():
 async def root(id:str):
     return {"message": f"Hello World {id}"}  # Return a JSON response with the dynamic "id"
 
-# downloadFileFromUrl.py
+# api ดาวน์โหลดรูปภาพจาก URL
 @app.get("/download/{image_url:path}")
 async def download_image(image_url: str):
     # ดาวน์โหลดรูปภาพจาก URL
@@ -20,11 +21,20 @@ async def download_image(image_url: str):
     
     # ตรวจสอบว่าการดาวน์โหลดเสร็จสิ้นและสำเร็จ
     if response.status_code == 200:
-        # สร้างการตอบสนองเป็น StreamingResponse และส่งข้อมูลไปยังการตอบสนอง
-        return StreamingResponse(iter(response.iter_content(chunk_size=128)), media_type="image/jpeg")
+        # สร้างไฟล์ชั่วคราวเพื่อเก็บข้อมูลรูปภาพ
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            for chunk in response.iter_content(chunk_size=128):
+                temp_file.write(chunk)
+        
+        # สร้างการตอบสนอง StreamingResponse โดยใช้ไฟล์ชั่วคราวที่สร้าง
+        file_name = "downloaded_image.jpg"  # ตั้งชื่อไฟล์ตามที่คุณต้องการ
+        headers = {"Content-Disposition": f"attachment; filename={file_name}"}
+        with open(temp_file.name, "rb") as file:
+            return StreamingResponse(iter([file.read()]), media_type="image/jpeg", headers=headers)
     else:
         # หากไม่สามารถดาวน์โหลดรูปภาพได้
         return {"error": "Unable to download image"}
 
 
 # uvicorn api:app --port 8001 --reload
+
